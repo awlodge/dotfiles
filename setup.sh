42022e6bc7c7fbb335e6e270494832ecf33694cc
Alex Lodge <awlodge@gmail.com>
2014-08-11 10:56:42 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -8,11 +8,13 @@ then
   exit 1
 fi
 
-backupdir="~/.backupdotfiles"
+backupdir="$HOME/.backupdotfiles"
 
 if [[ -d $backupdir ]]
 then
   echo Backup directory already exists. Remove before running script.
+  exit 1
+fi
 
 # Copy the existing file to the backup directory and copy in the new file.
 backup_and_copy()
@@ -20,13 +22,22 @@ backup_and_copy()
   filename=$1
   if [[ -e ~/$filename ]]
   then
-    cp ~/$filename ~/backupdotfiles/$filename
+    cp ~/$filename $backupdir/$filename
   fi
   cp $filename ~/$filename
 }
 
+# Link a script into ~/bin.
+softlink_to_bin()
+{
+  filename=$1
+  bin_filename=$(basename $filename | sed -e 's/\.sh$//')
+  ln -s $filename ~/bin/$bin_filename
+}
+export -f softlink_to_bin
+
 # Back up the old files
-mkdir ~/backupdotfiles
+mkdir $backupdir
 
 # Copy in the dot files
 backup_and_copy .bashrc
@@ -34,7 +45,4 @@ backup_and_copy .tmux.conf
 backup_and_copy .gitconfig
 
 # Softlink the scripts into ~/bin
-for ff in $(ls scripts)
-do
-  ln -s ${PWD}/scripts/$ff ~/bin/$(echo $ff | sed -e 's/\.sh$//')
-done
+find scripts -name "*.sh" -exec bash -c 'softlink_to_bin "$@"' bash {} \;
